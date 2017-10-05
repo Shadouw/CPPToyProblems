af226e5b58ef4988d6f5837cd47790b27ab66f7c
Shadouw <rolf.schatten@gmail.com>
2017-10-05 10:44:42 +0200

    Added Check-Prime

---
+++ Check-Prime/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+int main ( int argc, char *argv[] )
+{
+    assert ( argc == 2 );
+    long number = atol(argv[1]);
+
+    for ( long i=2; i<sqrt(number); ++i )
+    {
+        if ( 0 == number%i )
+        {
+            cout << "Not a prime" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Prime" << endl;
+
+    return 0;
+}
